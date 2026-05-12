a05ff6907ec2bdf436fb5dfbf21d3582cf2431c7
AlexanderYascov <Yascov64@gmail.com>
2026-05-12 23:28:42 +0300

    sklearn DecisionTree 2.0

--- main.py
+++ main.py
@@ -11,8 +11,8 @@ import make_logger
 import json
 import requests
 import ccxt
+from sklearn.model_selection import GridSearchCV
 from sklearn.tree import DecisionTreeClassifier
-from sklearn.model_selection import train_test_split
 from sklearn.tree import plot_tree
 
 current_file_path = Path(__file__).resolve()
@@ -87,8 +87,27 @@ def main():
 	vectorF1 = np.array(workDataFrame['ROC']).reshape(-1, 1)[uniCut:]
 	xTest = np.column_stack((vectorF0, vectorF1))
 
-	model = DecisionTreeClassifier(max_depth=3, random_state=42)
-	model.fit(xTrain, yTrain)
+	baseModel = DecisionTreeClassifier(random_state=42)
+
+	paramGrid = {
+		'max_depth': [3, 5, 10, None],
+		'min_samples_split': [2, 5, 10],
+		'min_samples_leaf': [1, 2, 4],
+		'criterion': ['gini', 'entropy'],
+		'max_features': [None, 'sqrt', 'log2']
+	}
+
+	gridSearch = GridSearchCV(
+		estimator=baseModel,
+		param_grid=paramGrid,
+		cv=5,                    # 5-кратная кросс-валидация
+		scoring='accuracy',      # метрика качества
+		n_jobs=-1,              # используем все ядра процессора
+		verbose=1               # показываем прогресс
+	)
+
+	gridSearch.fit(xTrain, yTrain)
+	model = gridSearch.best_estimator_
 
 	plt.figure(figsize=(12, 8))
 	plot_tree(model, 
