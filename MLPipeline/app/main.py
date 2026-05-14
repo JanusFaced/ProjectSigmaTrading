575d3ac4748629d537a984f073d14233263e4028
AlexanderYascov <Yascov64@gmail.com>
2026-05-14 21:57:17 +0300

    docker_compose DTree 2.3

--- MLPipeline/app/main.py
+++ MLPipeline/app/main.py
@@ -14,6 +14,7 @@ import ccxt
 from sklearn.model_selection import GridSearchCV
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.tree import plot_tree
+from sqlalchemy import create_engine
 
 current_file_path = Path(__file__).resolve()
 current_dir = current_file_path.parent
@@ -22,8 +23,27 @@ os.environ['LEVEL_CONFIG'] = 'INFO'
 os.environ['WAY_TO_LOG_JOURNAL'] = str(current_dir/'logs'/'log_journal.log')
 os.environ['WAY_EXTRACT_FILES'] = str(current_dir/'extract_files')
 
+dataBase_user = os.getenv('DB_USER', 'postgres')
+dataBase_password = os.getenv('DB_PASSWORD', 'postgres')
+dataBase_name = os.getenv('DB_NAME', 'postgres')
+
+dataBase_host = os.getenv('DB_HOST', 'dataBase')
+dataBase_port = os.getenv('DB_PORT', '5432')
+
 make_logger.make()
 logger = logging.getLogger('DATAMINER:dms')
+
+DATABASE_URL = f"postgresql://{dataBase_user}:{dataBase_password}@{dataBase_host}:{dataBase_port}/{dataBase_name}"
+engine = create_engine(DATABASE_URL)
+
+def checkConnect():
+	logger.info(f"Попытка подключения к БД на хосте: {dataBase_host}...")
+	try:
+		with engine.connect() as conn:
+			result = conn.execute("SELECT 1")
+			logger.info("✅ Подключение к базе данных успешно установлено!")
+	except Exception as e:
+		logger.info(f"❌ Ошибка подключения к БД: {e}")
 
 def dataFrameDownloader(symbol, nameExchange, timeFrame, startYear):
 	limit = 1000
@@ -45,22 +65,29 @@ def dataFrameDownloader(symbol, nameExchange, timeFrame, startYear):
 	dataFrame = dataFrame[['datetime', 'open', 'high', 'low', 'close', 'volume']]
 	return dataFrame
 
+'''
 def main():
 
-	dataFrame = dataFrameDownloader(symbol='BTC', nameExchange='binance', timeFrame='1d', startYear=2024)
+	dataFrame = dataFrameDownloader(symbol='ETH', nameExchange='binance', timeFrame='1d', startYear=2023)
 
-	window_ma = 10
-	window_roc = 100
+	windowFeatures0 = 7
+	windowFeatures1 = 30
+	windowFeatures2 = 90
 
-	dataFrame['MA'] = dataFrame['close'].rolling(window=window_ma).mean()
-	dataFrame['delta'] = (dataFrame['close'] - dataFrame['MA'])/dataFrame['MA']
+	centreMoving = 20
+	quantile = 0.75
 
-	dataFrame['ROC'] = (dataFrame['close'] - dataFrame['close'].shift(window_roc))/dataFrame['close'].shift(window_roc)
+	dataFrame['features0'] = (dataFrame['close'] - dataFrame['close'].shift(windowFeatures0))/dataFrame['close'].shift(windowFeatures0)
+	dataFrame['features1'] = (dataFrame['close'] - dataFrame['close'].shift(windowFeatures1))/dataFrame['close'].shift(windowFeatures1)
+	dataFrame['features2'] = (dataFrame['close'] - dataFrame['close'].shift(windowFeatures2))/dataFrame['close'].shift(windowFeatures2)
+
+	dataFrame['centreMoving'] = dataFrame['close'].rolling(window=centreMoving).mean().shift(-int(centreMoving/2))
+	dataFrame['diffCentreMoving'] = dataFrame['centreMoving'] - dataFrame['centreMoving'].shift(1)
 
 	dataFrame['classEDU'] = np.select(
 			[
-				(dataFrame['delta'] > 0) & (dataFrame['ROC'] > 0),
-				(dataFrame['delta'] < 0) & (dataFrame['ROC'] < 0)
+				(dataFrame['diffCentreMoving'] > 0),
+				(dataFrame['diffCentreMoving'] < 0)
 			],
 			[
 				-1,
@@ -69,30 +96,37 @@ def main():
 			default=0
 		)
 
-	workDataFrame = dataFrame.iloc[window_roc:]
+	plt.plot(dataFrame['datetime'], dataFrame['close'])
+	plt.plot(dataFrame['datetime'], dataFrame['centreMoving'])
+	plt.savefig(str(current_dir/'output'/'plotInd0.png'))
+	plt.close()
 
-	uniCut = int(len(workDataFrame)/2)
+	workDataFrame = dataFrame.iloc[windowFeatures2:]
+
+	uniCut = int(len(workDataFrame)*quantile)
 
 	datetimeTrain = workDataFrame['datetime'][:uniCut]
 	closeTrain = workDataFrame['close'][:uniCut]
 	yTrain = np.array(workDataFrame['classEDU'])[:uniCut]
-	vectorF0 = np.array(workDataFrame['delta']).reshape(-1, 1)[:uniCut]
-	vectorF1 = np.array(workDataFrame['ROC']).reshape(-1, 1)[:uniCut]
-	xTrain = np.column_stack((vectorF0, vectorF1))
+	vectorF0 = np.array(workDataFrame['features0']).reshape(-1, 1)[:uniCut]
+	vectorF1 = np.array(workDataFrame['features1']).reshape(-1, 1)[:uniCut]
+	vectorF2 = np.array(workDataFrame['features2']).reshape(-1, 1)[:uniCut]
+	xTrain = np.column_stack((vectorF0, vectorF1, vectorF2))
 
 	datetimeTest = workDataFrame['datetime'][uniCut:]
 	closeTest = workDataFrame['close'][uniCut:]
 	yTest = np.array(workDataFrame['classEDU'])[uniCut:]
-	vectorF0 = np.array(workDataFrame['delta']).reshape(-1, 1)[uniCut:]
-	vectorF1 = np.array(workDataFrame['ROC']).reshape(-1, 1)[uniCut:]
-	xTest = np.column_stack((vectorF0, vectorF1))
+	vectorF0 = np.array(workDataFrame['features0']).reshape(-1, 1)[uniCut:]
+	vectorF1 = np.array(workDataFrame['features1']).reshape(-1, 1)[uniCut:]
+	vectorF2 = np.array(workDataFrame['features2']).reshape(-1, 1)[uniCut:]
+	xTest = np.column_stack((vectorF0, vectorF1, vectorF2))
 
 	baseModel = DecisionTreeClassifier(random_state=42)
 
 	paramGrid = {
-		'max_depth': [3, 5, 10, None],
-		'min_samples_split': [2, 5, 10],
-		'min_samples_leaf': [1, 2, 4],
+		'max_depth': [3, 4, 5, None],
+		'min_samples_split': [2, 3, 4],
+		'min_samples_leaf': [1, 2, 3, 4],
 		'criterion': ['gini', 'entropy'],
 		'max_features': [None, 'sqrt', 'log2']
 	}
@@ -111,8 +145,8 @@ def main():
 
 	plt.figure(figsize=(12, 8))
 	plot_tree(model, 
-			  feature_names=['delta', 'ROC'],
-			  class_names=['Buy', 'Wait', 'Sell'],
+			  feature_names=['features0', 'features1', 'features2'],
+			  class_names=['Buy', 'Sell', 'Wait'],
 			  filled=True,
 			  rounded=True,
 			  fontsize=10)
@@ -122,16 +156,20 @@ def main():
 
 	yPredict = model.predict(xTrain)
 	plt.plot(datetimeTrain, closeTrain)
-	plt.plot(datetimeTrain, closeTrain*(1+yPredict/100))
+	plt.plot(datetimeTrain, closeTrain*(1+yPredict/25))
 	plt.savefig(str(current_dir/'output'/'plot1.png'))
 	plt.close()
 
 	yPredict = model.predict(xTest)
 	plt.plot(datetimeTest, closeTest)
-	plt.plot(datetimeTest, closeTest*(1+yPredict/100))
+	plt.plot(datetimeTest, closeTest*(1+yPredict/25))
 	plt.savefig(str(current_dir/'output'/'plot2.png'))
 	plt.close()
 
+	logger.info('End!')
+'''
+def main():
+	checkConnect()
 	logger.info('End!')
 
 try:
--- start.bat
+++ start.bat
@@ -1,10 +1,7 @@
 @echo off
 cd /d %~dp0
 
-echo Make image python_app!
-docker build -t python_app .
-
-echo Start container python_app!
-docker run --rm -v "%cd%/output:/app/output" python_app
+echo Start docker compose!
+docker-compose up --build
 
 pause
