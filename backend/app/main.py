24181f7a971847fbbcefaa93899903e0946169f3
AlexanderYascov <Yascov64@gmail.com>
2026-05-19 21:58:43 +0300

    fullStack 1.2

--- backend/app/main.py
+++ backend/app/main.py
@@ -25,6 +25,3 @@ tableAnalyst = data.main()
 @app.route('/getTableAnalyst', methods=['GET'])
 def getTableAnalyst():
     return jsonify(tableAnalyst)
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8000, debug=True)
