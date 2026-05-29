e8b30fb418d07cd31e9726cc339edce1d8a5bc96
AlexanderYascov <Yascov64@gmail.com>
2026-05-29 21:55:42 +0300

    PST 1.1.14 (darts.CatBoost for regression)

--- MLPipeline/src/plRegression.py
+++ MLPipeline/src/plRegression.py
@@ -5,7 +5,9 @@ import pandas as pd
 import numpy as np
 import os
 import sys
-from statsmodels.tsa.arima.model import ARIMA
+from darts.models import CatBoostModel
+from darts.metrics import smape
+from darts import TimeSeries
 from sklearn.metrics import accuracy_score
 from sklearn.metrics import mean_absolute_percentage_error
 from sklearn.metrics import r2_score
@@ -21,32 +23,68 @@ def main(inputMessage: dict[str, Any], dataFrame: pd.DataFrame) -> None:
 	amountCandles = 10
 	uniCut = len(dataFrame) - amountCandles
 
-	yDataEdu: NDArray[np.float64] = np.array(dataFrame['close'])[:uniCut]
+	dataFrameTrain: pd.DataFrame = dataFrame[:uniCut]
+	dataFrameTest: pd.DataFrame = dataFrame[uniCut:]
 
-	imageModel = ARIMA(yDataEdu, order=(10, 1, 1))
-	model = imageModel.fit()
+	seriesTrain = TimeSeries.from_dataframe(
+		dataFrameTrain,
+		time_col='datetime',
+		value_cols='close',
+		fill_missing_dates=True,
+		freq=timeFrame
+	)
+
+	seriesTest = TimeSeries.from_dataframe(
+		dataFrameTest,
+		time_col='datetime',
+		value_cols='close',
+		fill_missing_dates=True,
+		freq=timeFrame
+	)
+
+	param_grid = {
+		'lags': [5, 15],
+		'iterations': [200, 600],
+		'depth': [4, 8],
+		'learning_rate': [0.02, 0.08]
+	}
+
+	imageModel = CatBoostModel(
+		lags=5,
+		output_chunk_length=1,
+		loss_function='RMSE',
+		iterations=200,
+		depth=4,
+		learning_rate=0.02,
+		verbose=False
+	)
+
+	model, best_params, best_score = imageModel.gridsearch(
+		parameters=param_grid,
+		series=seriesTrain,
+		val_series=seriesTest,
+		metric=smape,
+		verbose=True
+	)
+
+	logger.info(f"Best parametrs: {best_params}")
+
+	model.fit(seriesTrain)
 
 	yDataTest: NDArray[np.float64] = np.array(dataFrame['close'])[uniCut:]
 
-	yPredict = model.forecast(steps=len(yDataTest))
-	print(f"Прогноз следующих значений: {yPredict}")
+	yPredictSeries = model.predict(n=len(yDataTest))
 
-
+	yPredict: NDArray[np.float64] = yPredictSeries.values().flatten()
 	mape = mean_absolute_percentage_error(yDataTest, yPredict)
 	r2 = r2_score(yDataTest, yPredict)
-
 	logger.info(f"Относительная ошибка: {100*mape:.4f} %")
 	logger.info(f"Коэффициент детерминации: {r2:.4f}")
 
-	yDataEdu = yDataEdu[-amountCandles:]
+	seriesTrain = seriesTrain[-amountCandles:]
 
-	lenth = len(yDataEdu)
-	time = range(len(yDataEdu) + len(yDataTest))
-
-	plt.plot(time[:lenth], yDataEdu, color="black")
-	plt.plot(time[lenth:], yDataTest, color="blue")
-	plt.plot(time[lenth:], yPredict, color="purple")
-	plt.savefig(
-		os.path.join("output/", f"predict_{symbol}_{timeFrame}.png"),
-	)
+	seriesTrain.plot(label='PastData', color="black")
+	seriesTest.plot(label='RealFutureData', color="blue")
+	yPredictSeries.plot(label='PredictFutureData', color="purple")
+	plt.savefig(os.path.join("output/", f"predict_{symbol}_{timeFrame}.png"))
 	plt.close()
